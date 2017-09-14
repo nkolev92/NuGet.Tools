6a095028b56f220220f43d463e302eb08e3e00d9
Nikolche Kolev <nikolev@microsoft.com>
2017-09-13 20:17:45 -0700

    initial work

---
+++ NuGet.Tools/NupkgAnalyzer/FileUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NupkgAnalyzer
+{
+    class FileUtility
+    {
+        public void CreateFile(string path, bool deleteIfExists)
+        {
+            var dir = Path.GetDirectoryName(path);
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            if (File.Exists(path) && deleteIfExists)
+            {
+                File.Delete(path);
+            }
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+        }
+
+        private void CreateDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
---
+++ NuGet.Tools/NupkgAnalyzer/PackageAnalyzer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using NuGet.Protocol;
+using NuGet.Versioning;
+
+namespace NupkgAnalyzer
+{
+    enum NuGetDirectoryStructure
+    {
+        V2, V3
+    }
+
+    public interface IProcessNupkgCommand
+    {
+        Dictionary<string, string> Act(LocalPackageInfo localPackage);
+    }
+
+
+    public class EnumeratePS1ScriptsInPackage : IProcessNupkgCommand
+    {
+        public Dictionary<string, string> Act( LocalPackageInfo localPackage)
+        {
+
+            var results = new Dictionary<string, string>(); ;
+            using (var archive = ZipFile.Open(localPackage.Path, ZipArchiveMode.Read))
+            {
+
+            }
+
+            return results;
+        }
+
+    }
+
+    internal class PackageAnalyzer
+    {
+        private string _nupkgsRootDirectory;
+        private NuGetDirectoryStructure _nuGetDirectoryStructure;
+        private string _logOutputPath;
+
+        public PackageAnalyzer(string nupkgsRootDirectory, NuGetDirectoryStructure nuGetDirectoryStructure, string logOutputPath)
+        {
+            _nupkgsRootDirectory = nupkgsRootDirectory;
+            _nuGetDirectoryStructure = nuGetDirectoryStructure;
+            _logOutputPath = logOutputPath;
+        }
+
+        IEnumerable<LocalPackageInfo> GetAllPackagesInFolder(string rootPath, NuGetDirectoryStructure nuGetDirectoryStructure)
+        {
+            switch (nuGetDirectoryStructure)
+            {
+                case NuGetDirectoryStructure.V2:
+                    return LocalFolderUtility.GetPackagesV2(root: rootPath, log: null);
+                case NuGetDirectoryStructure.V3:
+                    return LocalFolderUtility.GetPackagesV3(root: rootPath, log: null);
+            }
+
+            return new List<LocalPackageInfo>();
+        } 
+
+
+
+    }
+}
