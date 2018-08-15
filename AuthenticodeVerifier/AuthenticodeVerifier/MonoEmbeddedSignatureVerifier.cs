1bf53f5ea71e9bedc6fafccf7631ad120ed52522
Nikolche Kolev <nikolev@microsoft.com>
2018-08-14 22:16:18 -0700

    Add a simple mono and windows signature verifiers

---
+++ AuthenticodeVerifier/AuthenticodeVerifier/MonoEmbeddedSignatureVerifier.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+namespace AuthenticodeVerifier
+{
+    class MonoEmbeddedSignatureVerifier
+    {
+        public bool IsValid(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException("file path cannot be empty", nameof(filePath));
+            }
+
+            var assembly = Assembly.Load("Mono.Security, Version = 4.0.0.0, Culture = neutral, PublicKeyToken = 0738eb9f132ed756");
+            if (assembly != null)
+            {
+                var type = assembly.GetType("Mono.Security.Authenticode.AuthenticodeDeformatter");
+                if (type != null)
+                {
+                    var instance = Activator.CreateInstance(type, filePath);
+                    var method = type.GetMethod("IsTrusted");
+                    if (method != null)
+                    {
+                        bool isTrusted = (bool)method.Invoke(instance, null);
+                        LogReason(type, instance);
+                        return isTrusted;
+                    }
+                }
+            }
+
+            Console.WriteLine("Either the assembly could not be loaded or the method cannot be found.");
+
+            return false;
+        }
+
+        private void LogReason(Type type, object instance)
+        {
+            var myField = type.GetProperty("Reason");
+            if (myField != null)
+            {
+                Console.WriteLine("Reason {0}", myField.GetValue(instance));
+            }
+            else
+            {
+                Console.WriteLine("Reason cannot be found");
+            }
+        }
+    }
+}
---
+++ AuthenticodeVerifier/AuthenticodeVerifier/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AuthenticodeVerifier
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("You need to provide the full path to a path");
+                return;
+            }
+
+            var filePath = args[0];
+
+            if (args.Length >= 2)
+            {
+                var verifier = new WindowsEmbeddedSignatureVerifier();
+                Console.WriteLine("Windows IsTrusted {0}", verifier.IsValid(filePath));
+            } else
+            {
+                var verifier = new MonoEmbeddedSignatureVerifier();
+                Console.WriteLine("Mono IsTrusted {0}", verifier.IsValid(filePath));
+            }
+
+
+        }
+    }
+}
