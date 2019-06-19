185987ed4483bf88d22694e63698645b20444751
Nikolche Kolev <nikolev@microsoft.com>
2019-06-18 22:12:56 -0700

    work

---
+++ PackageExplorer/PackageExplorer/AnalyzedPackageCompatibility.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExplorer
+{
+    public class PackageCompatibilityInfo
+    {
+        public string Id { get; }
+        public string Version { get; }
+        public IEnumerable<string> AllFiles { get; set; }
+
+        public IEnumerable<string> DependencyGroupFrameworks { get; set; }
+
+        public IEnumerable<string> LibFrameworks { get; set; }
+        public IEnumerable<string> LibFiles { get; set; }
+
+
+        public IEnumerable<string> RefFrameworks { get; set; }
+        public IEnumerable<string> RefFiles { get; set; }
+
+
+        public IEnumerable<string> RuntimeFrameworks { get; set; }
+        public IEnumerable<string> RuntimeFiles { get; set; }
+
+
+        public IEnumerable<string> MSBuildMultiTargetingFrameworks { get; set; }
+        public IEnumerable<string> MSBuildMultiTargetingFiles { get; set; }
+
+
+        public IEnumerable<string> MSBuildFrameworks { get; set; }
+        public IEnumerable<string> MSBuildFiles { get; set; }
+
+
+        public IEnumerable<string> MSBuildTransitiveFrameworks { get; set; }
+        public IEnumerable<string> MSBuildTransitiveFiles { get; set; }
+
+
+        public IEnumerable<string> AnyTargetFrameworks { get; set; }
+        public IEnumerable<string> AnyTargetFiles { get; set; }
+
+
+        public PackageCompatibilityInfo(string id, string version)
+        {
+            Id = id;
+            Version = version;
+        }
+
+    }
+}
--- PackageExplorer/PackageExplorer/PackageAssetsCompatibilityAnalyzer.cs
+++ PackageExplorer/PackageExplorer/PackageAssetsCompatibilityAnalyzer.cs
@@ -1,33 +1,75 @@
 ﻿using NuGet.Client;
 using NuGet.ContentModel;
 using NuGet.Frameworks;
-using System;
-using System.Collections;
+using NuGet.Packaging;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PackageExplorer
 {
-    class PackageAssetsCompatibilityAnalyzer
+
+    public static class PackageAssetsCompatibilityAnalyzer
     {
-
-        public static IEnumerable<NuGetFramework> GetCompatibleFrameworks(IEnumerable<string> fileList, string folder)
+        public static void AnalyzePackageDependenciesCompatibility(this PackageCompatibilityInfo analyzedPackage, NuspecReader nuspecReader)
         {
+            if (nuspecReader != null)
+            {
+                analyzedPackage.DependencyGroupFrameworks = nuspecReader.GetDependencyGroups().Select(e => e.TargetFramework.GetShortFolderName());
+            }
+        }
 
-            if (fileList == null) return Enumerable.Empty<NuGetFramework>();
-            if (folder == null) return Enumerable.Empty<NuGetFramework>();
-            var managedCodeConventions = new ManagedCodeConventions(new NuGet.RuntimeModel.RuntimeGraph());
+        public static void AnalyzePackageContentCompatibility(this PackageCompatibilityInfo analyzedPackage, IEnumerable<string> fileList)
+        {
+            if (fileList != null)
+            {
+                var managedCodeConventions = new ManagedCodeConventions(new NuGet.RuntimeModel.RuntimeGraph());
+                var collection = new ContentItemCollection();
+                collection.Load(fileList);
 
-            var collection = new ContentItemCollection();
-            collection.Load(fileList);
+                var libItems = GetContentForPattern(collection, managedCodeConventions.Patterns.CompileLibAssemblies);
+                analyzedPackage.LibFrameworks = GetGroupFrameworks(libItems);
+                analyzedPackage.LibFiles = GetGroupFiles(libItems);
 
-            // Act
-            var groups = collection.FindItemGroups(managedCodeConventions.Patterns.RuntimeAssemblies)
-                .OrderBy(group => ((NuGetFramework)group.Properties["tfm"]).GetShortFolderName())
-                .ToList();
-            return groups.Select(e => (NuGetFramework)e.Properties["tfm"]);
+                var refItems = GetContentForPattern(collection, managedCodeConventions.Patterns.CompileRefAssemblies);
+                analyzedPackage.RefFrameworks = GetGroupFrameworks(refItems);
+                analyzedPackage.RefFiles = GetGroupFiles(refItems);
+
+                var runtimeItems = GetContentForPattern(collection, managedCodeConventions.Patterns.RuntimeAssemblies);
+                analyzedPackage.RuntimeFrameworks = GetGroupFrameworks(runtimeItems);
+                analyzedPackage.RuntimeFiles = GetGroupFiles(runtimeItems);
+
+                var msbuildItems = GetContentForPattern(collection, managedCodeConventions.Patterns.MSBuildFiles);
+                analyzedPackage.MSBuildFrameworks = GetGroupFrameworks(msbuildItems);
+                analyzedPackage.MSBuildFiles = GetGroupFiles(msbuildItems);
+
+                var msbuildMultiTargetingItems = GetContentForPattern(collection, managedCodeConventions.Patterns.MSBuildMultiTargetingFiles);
+                analyzedPackage.MSBuildMultiTargetingFrameworks = GetGroupFrameworks(msbuildMultiTargetingItems);
+                analyzedPackage.MSBuildMultiTargetingFiles = GetGroupFiles(msbuildMultiTargetingItems);
+
+                var msbuildTransitiveItems = GetContentForPattern(collection, managedCodeConventions.Patterns.MSBuildTransitiveFiles);
+                analyzedPackage.MSBuildTransitiveFrameworks = GetGroupFrameworks(msbuildTransitiveItems);
+                analyzedPackage.MSBuildTransitiveFiles = GetGroupFiles(msbuildTransitiveItems);
+
+                var anyItems = GetContentForPattern(collection, managedCodeConventions.Patterns.AnyTargettedFile);
+                analyzedPackage.AnyTargetFrameworks = GetGroupFrameworks(anyItems);
+                analyzedPackage.AnyTargetFiles = GetGroupFiles(anyItems);
+            }
+        }
+
+        private static IEnumerable<ContentItemGroup> GetContentForPattern(ContentItemCollection collection, PatternSet pattern)
+        {
+            return collection.FindItemGroups(pattern)
+                .OrderBy(group => ((NuGetFramework)group.Properties["tfm"]).GetShortFolderName());
+        }
+
+        private static IEnumerable<string> GetGroupFiles(IEnumerable<ContentItemGroup> groups)
+        {
+            return groups.Select(e => ((NuGetFramework)e.Properties["tfm"]).GetShortFolderName()).OrderBy(e => e);
+        }
+
+        private static IEnumerable<string> GetGroupFrameworks(IEnumerable<ContentItemGroup> groups)
+        {
+            return groups.SelectMany(e => e.Items).Select(e => e.Path).OrderBy(e => e);
         }
     }
 }
