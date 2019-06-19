9e3c141066bcc9e44173f629a8be0c6618a8e80f
Nikolche Kolev <nikolev@microsoft.com>
2019-06-18 17:03:10 -0700

    work

---
+++ PackageExplorer/PackageExplorer/PackageAssetsCompatibilityAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using NuGet.Client;
+using NuGet.ContentModel;
+using NuGet.Frameworks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExplorer
+{
+    class PackageAssetsCompatibilityAnalyzer
+    {
+
+        public static IEnumerable<NuGetFramework> GetCompatibleFrameworks(IEnumerable<string> fileList, string folder)
+        {
+
+            if (fileList == null) return Enumerable.Empty<NuGetFramework>();
+            if (folder == null) return Enumerable.Empty<NuGetFramework>();
+            var managedCodeConventions = new ManagedCodeConventions(new NuGet.RuntimeModel.RuntimeGraph());
+
+            var collection = new ContentItemCollection();
+            collection.Load(fileList);
+
+            // Act
+            var groups = collection.FindItemGroups(managedCodeConventions.Patterns.RuntimeAssemblies)
+                .OrderBy(group => ((NuGetFramework)group.Properties["tfm"]).GetShortFolderName())
+                .ToList();
+            return groups.Select(e => (NuGetFramework)e.Properties["tfm"]);
+        }
+    }
+}
