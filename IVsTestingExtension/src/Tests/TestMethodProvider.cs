af87c4d2eb1e42f20ffdf27311457ba09451a099
Nikolche Kolev <nikolev@microsoft.com>
2020-02-19 02:16:11 -0800

    stuff

--- IVsTestingExtension/src/Tests/TestMethodProvider.cs
+++ IVsTestingExtension/src/Tests/TestMethodProvider.cs
@@ -1,9 +1,17 @@
 ﻿using EnvDTE;
+using Microsoft;
+using Microsoft.ServiceHub.Framework;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.RpcContracts.OutputChannel;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.ServiceBroker;
 using NuGet.VisualStudio;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Threading;
 using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
 
 namespace IVsTestingExtension.Tests
 {
@@ -17,17 +25,30 @@ namespace IVsTestingExtension.Tests
 
         private async Task TestSyncInstallPackage(Project projectSelected, Dictionary<string, string> arguments)
         {
-            arguments.TryGetValue("packageId", out string packageId);
-            arguments.TryGetValue("packageVersion", out string packageVersion);
-            arguments.TryGetValue("source", out string source);
-            arguments.TryGetValue("ignoreDependencies", out string ignoreDependenciesStr);
-            bool.TryParse(ignoreDependenciesStr, out bool ignoreDependencies);
+            var service = await AcquireServiceAsync(CancellationToken.None, AsyncServiceProvider.GlobalProvider);
+            service.WriteLineAsync("myChannel", "We are writing a message!", CancellationToken.None);
+            //arguments.TryGetValue("packageId", out string packageId);
+            //arguments.TryGetValue("packageVersion", out string packageVersion);
+            //arguments.TryGetValue("source", out string source);
+            //arguments.TryGetValue("ignoreDependencies", out string ignoreDependenciesStr);
+            //bool.TryParse(ignoreDependenciesStr, out bool ignoreDependencies);
 
-            VsAsyncPackageInstaller.InstallPackage(source: source,
-                                              projectSelected,
-                                              packageId,
-                                              packageVersion,
-                                              ignoreDependencies: ignoreDependencies);
+            //VsAsyncPackageInstaller.InstallPackage(source: source,
+            //                                  projectSelected,
+            //                                  packageId,
+            //                                  packageVersion,
+            //                                  ignoreDependencies: ignoreDependencies);
+        }
+
+        private async Task<IOutputChannelStore> AcquireServiceAsync(CancellationToken cancellationToken, IAsyncServiceProvider asyncServiceProvider)
+        {
+            ServiceActivationOptions options = new ServiceActivationOptions();
+            options.SetClientDefaults();
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            IBrokeredServiceContainer brokeredServiceContainer = await asyncServiceProvider.GetServiceAsync<SVsBrokeredServiceContainer, IBrokeredServiceContainer>();
+            Assumes.Present(brokeredServiceContainer);
+            IServiceBroker sb = brokeredServiceContainer.GetFullAccessServiceBroker();
+            return await sb.GetProxyAsync<IOutputChannelStore>(VisualStudioServices.VS2019_4.OutputChannelStore, options, cancellationToken);
         }
     }
 }
